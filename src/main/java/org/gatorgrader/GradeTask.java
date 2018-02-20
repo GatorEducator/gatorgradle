d7f9a6615fab974005f431d129ba27c42d003831
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2018-02-20 15:47:46 -0500

    added base starting source files, almost empty travis config

---
+++ src/main/java/org/gatorgrader/GradeTask.java
@@ -0,0 +1,10 @@
+package org.gatorgrader;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class GradeTask extends DefaultTask {
+	
+
+
+}
---
+++ src/main/java/org/gatorgrader/RunGatorGraderPlugin.java
@@ -0,0 +1,12 @@
+package org.gatorgrader;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class RunGatorGraderPlugin extends Plugin<Project> {
+	
+	public void apply(Project project) {
+
+	}
+
+}
