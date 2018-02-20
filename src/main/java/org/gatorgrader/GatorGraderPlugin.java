1b60e6b9593bf803ae698225ba09dbefc6c992be
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2018-02-20 17:00:59 -0500

    finished basic skeleton, need implementation work in GradeTask

--- src/main/java/org/gatorgrader/GatorGraderPlugin.java
+++ src/main/java/org/gatorgrader/GatorGraderPlugin.java
@@ -4,12 +4,8 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 public class GatorGraderPlugin implements Plugin<Project> {
-	
-	public void apply(Project project) {
-		project.getTasks().create("grade", GradeTask.class, (task) -> {
-			task.setMessage("Hello");
-			task.setRecipient("World");
-		});
-	}
-
+    public void apply(Project project) {
+        GradeTask grade = project.getTasks().create("grade", GradeTask.class);
+        grade.dependsOn("clean").dependsOn("check").dependsOn("build");
+    }
 }
--- src/main/java/org/gatorgrader/GradeTask.java
+++ src/main/java/org/gatorgrader/GradeTask.java
@@ -4,29 +4,8 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 
 public class GradeTask extends DefaultTask {
-	private String message;
-	private String recipient;
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getRecipient() {
-		return recipient;
-	}
-
-	public void setRecipient(String recipient) {
-		this.recipient = recipient;
-	}
-
-	@TaskAction
-	public void sayGreeting() {
-		System.out.printf("%s, %s!\n", getMessage(), getRecipient());
-	}
-
-
+    @TaskAction
+    public void executeTask() {
+        System.out.println("Hello, Work! Doing grading...");
+    }
 }
