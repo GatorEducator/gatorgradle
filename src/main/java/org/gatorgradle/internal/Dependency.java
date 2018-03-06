839ae6081af7ac31b58211e745073c28ff764c57
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2018-03-06 01:06:59 -0500

    clean up for gh-pages

--- src/main/java/org/gatorgradle/internal/Dependency.java
+++
@@ -1,5 +0,0 @@
-package org.gatorgradle.internal;
-
-// TODO: add a way to dynamically load code here to download and install a dependency,
-//       to allow for arbitrary dependencies
-public enum Dependency { GATORGRADER, PYTHON, MDL, HTMLHINT }
