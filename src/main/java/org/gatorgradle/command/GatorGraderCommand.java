1ecf81e71757a1cb8868a652bb30982371563458
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2018-08-23 14:43:10 -0400

    add json flag to GatorGraderCommand, move flags to static variables

--- src/main/java/org/gatorgradle/command/GatorGraderCommand.java
+++ src/main/java/org/gatorgradle/command/GatorGraderCommand.java
@@ -3,24 +3,33 @@ package org.gatorgradle.command;
 import static org.gatorgradle.GatorGradlePlugin.F_SEP;
 import static org.gatorgradle.GatorGradlePlugin.GATORGRADER_HOME;
 
+import org.gatorgradle.internal.DependencyManager;
+
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * GatorGraderCommand automatically adds the python and gatorgrader path to the beginning of the
  * command.
- * TODO: make python3 a Dependency, create method to get default executable path in
- * DependencyManager
  */
 public class GatorGraderCommand extends BasicCommand {
     private static final long serialVersionUID = 2142L;
+
+    private static final String WELCOME_FLAG = "--nowelcome";
+    private static final String JSON_FLAG    = "--json";
 
     public GatorGraderCommand(String... command) {
         this(Arrays.asList(command));
     }
 
+    /**
+     * Create a command from a list of strings.
+     *
+     * @param command the list of arguments
+     **/
     public GatorGraderCommand(List<String> command) {
-        super("python3", GATORGRADER_HOME + F_SEP + "gatorgrader.py", "--nowelcome");
+        super(DependencyManager.getPython(), GATORGRADER_HOME + F_SEP + "gatorgrader.py",
+            WELCOME_FLAG, JSON_FLAG);
         super.with(command);
     }
 }
