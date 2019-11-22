1e4feae15f812d957f36de101fff2f3cfcaf4fbd
enpuyou <youe2@allegheny.edu>
2019-11-22 14:54:28 -0500

    move readFile to a new Read class in Util

---
+++ src/main/java/org/gatorgradle/util/Read.java
@@ -0,0 +1,27 @@
+package org.gatorgradle.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Read {
+
+
+  /**
+   * Read in reflection file
+   *
+   * @param fileName the path to the reflection file
+   * @return the reflection file in string with json escaped
+   */
+  public static String readFile(String fileName) throws IOException {
+    try {
+      String reflection = String.join("\n", Files.readAllLines(Paths.get(fileName)));
+      return StringUtil.jsonEscape(reflection);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
