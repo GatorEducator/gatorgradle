354622fbb13b0af95cc69e6f9f2aed0e09678516
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-12-12 18:41:10 -0500

    Remove some unused imports in GatorGradleReportTask
    This fixes some Codacy issues

--- src/main/java/org/gatorgradle/task/GatorGradleReportTask.java
+++ src/main/java/org/gatorgradle/task/GatorGradleReportTask.java
@@ -1,17 +1,8 @@
 package org.gatorgradle.task;
 
-import org.gatorgradle.command.BasicCommand;
-import org.gatorgradle.command.Command;
-import org.gatorgradle.config.GatorGradleConfig;
-import org.gatorgradle.display.CommandOutputSummary;
-import org.gatorgradle.internal.DependencyManager;
-import org.gatorgradle.internal.ProgressLoggerWrapper;
 import org.gatorgradle.task.GatorGradleTask;
-import org.gatorgradle.util.Console;
 
-import org.gradle.api.GradleException;
 import org.gradle.api.tasks.TaskAction;
-import org.gradle.workers.IsolationMode;
 
 public class GatorGradleReportTask extends GatorGradleTask {
 
