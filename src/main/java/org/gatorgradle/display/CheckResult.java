e0b528e5120008abbafcec40bb9688e858b6a5fa
Megan Corletti <corlettim@allegheny.edu>
2019-12-27 11:37:25 -0500

    add reach symbol with yellow color

--- src/main/java/org/gatorgradle/display/CheckResult.java
+++ src/main/java/org/gatorgradle/display/CheckResult.java
@@ -31,6 +31,7 @@ public class CheckResult {
   public static final String PASS_SYMBOL_RAW = "\u2714"; // heavy check (✔)
   public static final String FAIL_SYMBOL_RAW = "\u2718"; // heavy cross (✘)
   public static final String FIX_SYMBOL_RAW = "\u2794"; // right arrow (➔)
+  public static final String REACH_SYMBOL_RAW = "\u25CF"; // circle (●)
 
   public static final String PASS_SYMBOL =
       StringUtil.color(StringUtil.GOOD, PASS_SYMBOL_RAW);
@@ -38,6 +39,8 @@ public class CheckResult {
       StringUtil.color(StringUtil.BAD, FAIL_SYMBOL_RAW);
   public static final String FIX_SYMBOL =
       StringUtil.color("\u001B[1;33m", FIX_SYMBOL_RAW);
+  public static final String FIX_SYMBOL =
+      StringUtil.color("\u001B[33m", REACH_SYMBOL_RAW);
 
   public String check;
   public Boolean outcome;
